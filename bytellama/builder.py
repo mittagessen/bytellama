f04ff71ca52d30507557b95618340301801e5e74
Benjamin Kiessling <mittagessen@l.unchti.me>
2024-10-31 16:25:23 +0100

    Initial commit

---
+++ bytellama/builder.py
@@ -0,0 +1,28 @@
+from torchtune.models.llama3_2._component_builders import llama3_2
+from torchtune.modules import TransformerDecoder
+
+from bytellama.tokenizer import OctetTokenizer
+
+
+def octet_tokenizer(*args, **kwargs) -> OctetTokenizer
+    return OctetTokenizer()
+
+
+def byte_llama() -> TransformerDecoder:
+    """
+    Builder for creating a Llama3.2 model initialized w/ ~100M parameters
+
+    Returns:
+        TransformerDecoder: Instantiation of Llama3.2 100M model
+    """
+    return llama3_2(vocab_size=259,
+                    num_layers=12,
+                    num_heads=16,
+                    num_kv_heads=4,
+                    embed_dim=1024,
+                    max_seq_len=131072,
+                    intermediate_dim=5632,
+                    attn_dropout=0.0,
+                    norm_eps=1e-5,
+                    rope_base=500_000,
+                    scale_factor=32)
---
+++ bytellama/tokenizer.py
@@ -0,0 +1,62 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import List, Optional
+
+from torchtune.modules.tokenizers._utils import BaseTokenizer
+
+class OctetTokenizer(BaseTokenizer):
+    """
+    A non-trainable tokenizer that simple encodes strings as UTF-8 and uses
+    their octets.
+
+    Examples:
+        >>> tokenizer = OctetTokenizer()
+        >>> tokenized_text = tokenizer.encode("Hello world!", add_bos=True, add_eos=True)
+        >>> print(tokenized_text)
+        [1, 31587, 29644, 102, 2]
+    """
+
+    def __init__(self,):
+        self.pad_id = 0
+        self.bos_id = 1
+        self.eos_id = 2
+        self._offset = 3
+
+    def encode(self,
+               text: str,
+               add_bos: bool = True,
+               add_eos: bool = True) -> List[int]:
+        """
+        Encode text into token IDs.
+
+        Args:
+            text (str): The input text to be encoded, unbatched.
+            add_bos (bool): Whether to prepend BOS to the input, defaults to True.
+            add_eos (bool): Whether to append EOS to the input, defaults to True.
+
+        Returns:
+            List[int]: The encoded token IDs.
+        """
+        tokens = []
+        if add_bos:
+            tokens.append(self.bos_id)
+        tokens.extend([i + self._offset for i in text.encode("utf-8")])
+        if add_eos:
+            tokens.append(self.eos_id)
+        return tokens
+
+    def decode(self, ids: List[int]) -> str:
+        """Decode token IDs to strings.
+
+        Args:
+            ids (List[int]): The input token IDs to be decoded.
+
+        Returns:
+            str: The decoded text.
+        """
+        string = bytes([x - self._offset for x in ids]).decode("utf-8", errors="ignore")
+        return string
